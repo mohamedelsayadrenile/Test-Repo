25d680c78a7764428113387813c73c392b2cab2a
Mohamed Elsayad <mohamed.elsayed@renile.net>
2026-06-29 07:13:01 +0000

    update

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from test!")
+from faster_whisper import WhisperModel
 
+model = WhisperModel(
+    "Systran/faster-whisper-large-v3",
+    device="cuda",
+    compute_type="float16"
+)
 
-if __name__ == "__main__":
-    main()
+segments, info = model.transcribe(
+    "5913610109015039607.wav",
+    language="ar",
+    beam_size=5,
+    vad_filter=True,
+    condition_on_previous_text=False
+)
+
+for s in segments:
+    print(s.text)
